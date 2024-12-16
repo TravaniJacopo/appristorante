f815c150cac37b59134e7b76e217960e7fec164c
travanijacopo <127383977+TravaniJacopo@users.noreply.github.com>
2024-12-16 13:47:52 +0100

    Add files via upload

---
+++ aggiunta.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="aggiuntastyle.css">    
+</head>
+<body>
+<div class="grid-container">
+<div class="item1">id_cameriere</div>   
+
+<div class="item2">n_tavolo</div>
+
+<div class="item3">2</div>
+
+<div class="item4">22</div>
+
+<div class="item5"><button>BEVANDE</button></div>
+
+<div class="item6"><button>ANTIPASTI</button></div>
+
+<div class="item7"><button>PRIMI</button></div>
+
+<div class="item8"><button>SECONDI</button></div>
+
+<div class="item9"><button>DOLCI</button></div>
+
+<div class="item10">COMANDA</div>
+
+<div class="item11"><textarea></textarea></div>
+
+<div class="item12"><button style="background-color:red">ANNULLA</button></div>
+
+<div class="item13"><button style="background-color:red" >CONFERMA</button></div>
+</div>
+</body>
+</html>
